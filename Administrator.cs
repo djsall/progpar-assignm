76a7bcade62e5fac88e70b3ef1286698250e7548
Proksa Levente <proksalevente@gmail.com>
2019-10-28 20:28:39 +0100

    kezdek fáradni, biztonság kedvéért

---
+++ Administrator.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+namespace Beadando {
+	class Administrator {
+		DB db = new DB();
+		public void addAdministrator() {
+			string username, password, passwordVerify;
+			do {
+				WriteLine("Adja meg a regisztrálni kívánt felhasználónevet! ");
+				username = ReadLine();
+			} while (!isAlphaNum(username));
+			do {
+				WriteLine("Adja meg a fiókhoz társítani kívánt jelszót! ");
+				password = ReadLine();
+			} while (!isAlphaNum(password));
+			do {
+				WriteLine("Erősítse meg a társítani kíánt jelszót! ");
+				passwordVerify = ReadLine();
+			} while (!isAlphaNum(passwordVerify) && password != passwordVerify);
+			db.insertNewUser(username, password);
+		}
+		//azért bool, hogy ha sikeres a bejelentkezés, onnantól adminland-ba dobhassuk egyből, ne kelljen minden lépéshez újra bejelentkezni
+		//majd implementálja az aki nem szenved alváshiányban jelenleg
+		public bool login(){
+			string username, password;
+			do {
+				WriteLine("Adja meg a felhasználónevét! ");
+				username = ReadLine();
+				WriteLine("Adja meg a jelszavát! ");
+				password = ReadLine();
+			} while (!isAlphaNum(username) && !db.containsUser(username, password));
+			WriteLine("Sikeres bejelentkezés!");
+			return true;
+		}
+		bool isAlphaNum(string s){
+			if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+				return false;
+			for (int i = 0; i < s.Length; i++) {
+				if (!(char.IsLetterOrDigit(s[i])))
+					return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ DB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beadando {
+	class DB {
+		public bool containsUser(string u, string p){
+			throw new NotImplementedException("nincs még adatbázis faszfej");
+			//visszaadja hogy benne van e az adatbázisban a felhasználó
+		}
+		public bool insert(string s){
+			throw new NotImplementedException("nincs még adatbázis faszfej");
+			//igaz ha sikeres a beszúrás
+		}
+		public bool remove(string s){
+			throw new NotImplementedException("nincs még adatbázis faszfej");
+			//igaz ha sikeres a törlés
+		}
+		public bool insertNewUser(string u, string p){
+			//lehet hogy nem lesz praktikus a külön insert és insert user ha rendes adatbázist használunk, nem fájlt.
+			string data = string.Format("{0}\t{1}\t{2}\n", u, p, DateTime.Now.ToString());
+			return insert(data);
+		}
+	}
+}
