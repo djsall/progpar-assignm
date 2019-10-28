448cac0caeb65613ce789f5ff9b829dabee7b136
Proksa Levente <proksalevente@gmail.com>
2019-10-28 21:01:51 +0100

    it gets better with time

--- DB.cs
+++ DB.cs
@@ -4,14 +4,10 @@ using System.Text;
 
 namespace Beadando {
 	class DB {
-		//visszaadja hogy benne van e az adatbázisban a felhasználó
-		public bool containsUser(string username, string password){
+		//igaz ha az adatbázisban van ilyen felhasználó
+		//out: admin flag, igaz ha admin az emberünk
+		public bool containsUser(string username, string password, out bool isAdmin){
 			throw new NotImplementedException("nincs még adatbázis faszfej");
-		}
-		//megnézi a negyedik mezőt (3. index) a sorban, hogy igaz e.
-		public bool isUserAdmin(string username){
-			throw new NotImplementedException("nincs még adatbázis faszfej");
-
 		}
 		//igaz ha sikeres a beszúrás
 		public bool insert(string s){
@@ -23,7 +19,7 @@ namespace Beadando {
 		}
 		//lehet hogy nem lesz praktikus a külön insert és insert user ha rendes adatbázist használunk, nem fájlt.
 		public bool insertNewUser(string username, string password, bool isAdmin){
-			string data = string.Format("{0}\t{1}\t{2}\t{3}\n", username, password, DateTime.Now.ToString(), isAdmin.ToString());
+			string data = string.Format("{0}\t{1}\t{2}\n", username, password, isAdmin.ToString());
 			return insert(data);
 		}
 	}
---
+++ LoginHandler.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+namespace Beadando {
+	class LoginHandler {
+		DB db = new DB();
+		public void addAdministrator() {
+			string[] result = registration();
+			db.insertNewUser(result[0], result[1], true);
+		}
+		public void addUser(){
+			string[] result = registration();
+			db.insertNewUser(result[0], result[1], false);
+		}
+		//sikeres bejelentkezés, vagy nem, majd eldönti
+		public bool login(){
+			bool isAdmin;
+			string username, password;
+			do {
+				WriteLine("Adja meg a felhasználónevét! ");
+				username = ReadLine();
+				WriteLine("Adja meg a jelszavát! ");
+				password = ReadLine();
+			} while (!isAlphaNum(username) && !db.containsUser(username, password, out isAdmin));
+			WriteLine("Sikeres bejelentkezés!");
+			return true;
+		}
+		public string[] registration(){
+			string username, password, passwordVerify;
+			do {
+				WriteLine("Adja meg a regisztrálni kívánt felhasználónevet! ");
+				username = ReadLine();
+			} while (!isAlphaNum(username));
+			do {
+				WriteLine("Adja meg a fiókhoz társítani kívánt jelszót! ");
+				password = ReadLine();
+			} while (!isAlphaNum(password));
+			do {
+				WriteLine("Erősítse meg a társítani kíánt jelszót! ");
+				passwordVerify = ReadLine();
+			} while (!isAlphaNum(passwordVerify) && password != passwordVerify);
+			string[] result = new string[2];
+			result[0] = username;
+			result[1] = password;
+			return result;
+		}
+		bool isAlphaNum(string s){
+			if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+				return false;
+			for (int i = 0; i < s.Length; i++) {
+				if (!(char.IsLetterOrDigit(s[i])))
+					return false;
+			}
+			return true;
+		}
+	}
+}
