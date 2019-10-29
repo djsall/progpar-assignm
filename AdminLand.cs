c4f163ff5f3c358d789d3ec8d3d986ef4e1d9fb3
Proksa Levente <proksalevente@gmail.com>
2019-10-29 13:35:04 +0100

    Starting admin interface

---
+++ AdminLand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+namespace Beadando
+{
+	public class AdminLand
+	{
+		LoginHandler loginH = new LoginHandler();
+		public AdminLand()
+		{
+			WriteLine("Üdvözlöm az adminisztrációs kezelőfelületen!");
+			int menuOption = 3;
+			do
+			{
+				WriteLine("1 - Bejelentkezés");
+				WriteLine("2 - Regisztráció");
+				WriteLine("0 - Kilépés");
+			} while (menuOption < 0 && menuOption > 2);
+
+		}
+		void adminLogin()
+		{
+			bool result;
+			do
+			{
+				result = loginH.login();
+			} while (!result);
+			Clear();
+			WriteLine("Sikeres bejelentkezés!");
+		}
+	}
+}
