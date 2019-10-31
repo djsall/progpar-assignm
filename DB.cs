b778284b769bdb2d9a68be0d34d5a4cc65b3edb2
Proksa Levente <proksalevente@gmail.com>
2019-10-31 18:41:19 +0100

    Deciding on populating movies #1

--- DB.cs
+++ DB.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Beadando_Forms {
+	struct movie {
+		int year;
+		string director, title;
+	};
 	class DB {
 		public void createAdmin(string county, string city, string street, string cinemaName, string maintainerName, int houseNumber){
 			
 		}
 		public void saveMovies(int week, string[] movies){
-
+			//loop movies-ra, soronként és tabonként bontva a tulajdonságokat átadja adatbázis feltöltő metódusnak
 		}
 	}
 }
