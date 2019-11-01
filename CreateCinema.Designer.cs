01e0a1c083c96281a246f58ef110fa73845c4d12
Proksa Levente <proksalevente@gmail.com>
2019-11-01 13:49:22 +0100

    Finished saveMovies with some(tm) error handling, created pushToDb entripoint method for database

--- CreateCinema.Designer.cs
+++ CreateCinema.Designer.cs
@@ -39,7 +39,6 @@
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-			this.textBox5 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -174,19 +173,11 @@
 			// 
 			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
-			// textBox5
-			// 
-			this.textBox5.Location = new System.Drawing.Point(338, 215);
-			this.textBox5.Name = "textBox5";
-			this.textBox5.Size = new System.Drawing.Size(124, 20);
-			this.textBox5.TabIndex = 17;
-			// 
 			// CreateCinema
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.textBox5);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
@@ -227,6 +218,5 @@
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
-		private System.Windows.Forms.TextBox textBox5;
 	}
 }
--- DB.cs
+++ DB.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Beadando_Forms {
-	struct movie {
-		int year;
-		string director, title;
-	};
 	class DB {
 		public void createAdmin(string county, string city, string street, string cinemaName, string maintainerName, int houseNumber){
 			
 		}
+		public void pushToDb(int week, string genres, string starring, int playtime, string producer, string title, string ScreeningDate, string ScreeningTime, int ageRestriction){
+		//DATABASE ENTRY-POINT
+		//ha a megfelelő hétbe jönnek az adatok, akkor mentsük el az adatbázisba az adatokat
+		}
 		public void saveMovies(int week, string[] movies){
 			foreach (var item in movies) {
 				string[] line = item.Split('\t');
+				string genres = line[0],
+							 starring = line[1], 
+							 producer = line[3], 
+							 title = line[4], 
+							 ScreeningDate = line[5], 
+							 ScreeningTime = line[6];
+
+				int playtime, ageRestriction;
+				bool playtimeCheck = int.TryParse(line[2], out playtime),
+						 ageRestrictionCheck = int.TryParse(line[7], out ageRestriction);
+				if (playtimeCheck && ageRestrictionCheck) 
+					pushToDb(week, genres, starring, playtime, producer, title, ScreeningDate, ScreeningTime, ageRestriction);
+				else 
+					MessageBox.Show("Számérték helyén más található a feldolgozandó fájlban. Kérem javítsa!");
 			}
-			//loop movies-ra, soronként és tabonként bontva a tulajdonságokat átadja adatbázis feltöltő metódusnak
 		}
 	}
 }
