be2917a3e312756273c178bef3d48566347ae7c2
akvgergo <akvgergo@gmail.com>
2019-11-16 16:29:13 +0100

    Sanitizing admin register

--- AdminRegister.cs
+++ AdminRegister.cs
@@ -11,7 +11,20 @@ namespace Beadando_Forms {
 			string username = textBox1.Text,
 						 password = textBox2.Text,
 						 cPassword = textBox3.Text;
-			if (password == cPassword){ 
+
+            if (string.IsNullOrWhiteSpace(username) || username.Length < 3)
+            {
+                MessageBox.Show("A felhasználónév nem álhat csak szóköz karakterekből, valamint legalább 3 karakter hosszúnak kell lennie.", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 5)
+            {
+                MessageBox.Show("A Jelszó nem álhat csak szóköz karakterekből, valamint legalább 5 karakter hosszúnak kell lennie.", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (password == cPassword){ 
 				if (DB.registerAdmin(username, password))
 					MessageBox.Show("Sikeres regisztráció!\nLépjen vissza, majd lépjen be és töltse fel a mozija adatait!", "Sikeres regisztráció!", MessageBoxButtons.OK, MessageBoxIcon.Information);
 			}else
